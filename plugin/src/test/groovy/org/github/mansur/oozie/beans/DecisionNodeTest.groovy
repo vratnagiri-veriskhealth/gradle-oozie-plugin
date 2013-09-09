eae0d3c0460ff4821f7bc13d42f3c1420f58ff7b
Robertson, Ian <ian.robertson@veriskhealth.com>
2013-09-09 15:36:26 -0600

    test buildXml

--- plugin/src/test/groovy/org/github/mansur/oozie/beans/DecisionNodeTest.groovy
+++ plugin/src/test/groovy/org/github/mansur/oozie/beans/DecisionNodeTest.groovy
@@ -1,21 +1,36 @@
 package org.github.mansur.oozie.beans;
 
 import static org.junit.Assert.*;
+import static BuilderTestUtils.assertXml
 
 import org.junit.Test;
 
 class DecisionNodeTest {
+  private final static DecisionNode node = new DecisionNode(
+        name: 'choose',
+        cases: [ new DecisionCaseNode(to: 'one', condition: 'odd'),
+                 new DecisionCaseNode(to: 'zero', condition: 'even') ],
+        defaultCase: 'huh?')
 
   @Test
   public void testToMap() {
     assertEquals(
       [type: 'decision', name: 'choose', switch: [[to: 'one', if: 'odd'], [to: 'zero', if: 'even']], default: 'huh?'],
-      new DecisionNode(
-        name: 'choose',
-        cases: [ new DecisionCaseNode(to: 'one', condition: 'odd'),
-                 new DecisionCaseNode(to: 'zero', condition: 'even') ],
-        defaultCase: 'huh?').toMap()
+      node.toMap()
     )
   }
 
+  @Test
+  public void testBuildXml() {
+    assertXml(node, """
+  <decision name='choose'>
+    <switch>
+      <case to='one'>odd</case>
+      <case to='zero'>even</case>
+      <default to="huh?"/>
+    </switch>
+  </decision>
+""")
+
+  }
 }
